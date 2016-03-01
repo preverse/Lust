41ccab18f1d120e95aa25f8cec2cf9d54686ac08
Pedro Liberato <ptribal@gmail.com>
2016-03-01 14:25:50 +0100

    Add solution

---
+++ Lust/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lust.Startup))]
+namespace Lust
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
